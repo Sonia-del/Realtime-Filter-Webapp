cac4b0111755d91f3514209ed6fa54aaf9f78184
Sonia-del <66464686+Sonia-del@users.noreply.github.com>
2021-02-12 17:57:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+Nose_x = 0;
+Nose_y = 0;
+
 function preload()
 {
 
@@ -19,19 +22,24 @@ function modelLoaded()
     console.log("PoseNet is Initialize");
 }
 
-function gotPoses(reuslts)
+function gotPoses(results)
 {
-    if(reuslts.length > 0)
+    if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        console.log("Nose_X" + Nose_x);
+        console.log("Nose_Y" + Nose_y);
+        Nose_x = results[0].pose.nose.x;
+        Nose_y = results[0].pose.nose.y;
     }
 }
 
 function draw()
 {
     image(video, 0, 0, 300, 300 );
+    circle(Nose_x, Nose_y, 20);
+    stroke(255, 0, 0);
+    fill(255, 0, 0);
 }
 
 function take_snapshot()
